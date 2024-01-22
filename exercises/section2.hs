d2cf57ded4cc8aff6e1f588816435e9731bf140e
leandro.quinterio <leandro.quinterio@rebase.com.br>
2024-01-22 09:54:51 -0300

    wip: section2 items b - c

--- exercises/section2.hs
+++ exercises/section2.hs
@@ -3,15 +3,37 @@ module Section2 where
     ===== Item 2.1 =====
   -}
 
-  data Month = January | February | March| April | May | June
-              | July | August | September | October | November | December 
+  data Month = January | February | March | April | May | June
+              | July | August | September | October | November | December deriving Show
 
   -- a) retorna o número de dias que cada mês possui (considere fevereiro tendo 28 dias).
-  checaFim :: Month -> Int
-  checaFim February = 28
-  checaFim April = 30
-  checaFim June = 30
-  checaFim September = 30
-  checaFim November = 30
-  checaFim _ = 31
+  monthLastDay :: Month -> Int
+  monthLastDay February = 28
+  monthLastDay April = 30
+  monthLastDay June = 30
+  monthLastDay September = 30
+  monthLastDay November = 30
+  monthLastDay _ = 31
 
+  -- b) recebe um mês atual e retorna o próximo mês
+  nextMonth :: Month -> Month
+  nextMonth January = February
+  nextMonth February = March
+  nextMonth March = April
+  nextMonth April = May
+  nextMonth May = June
+  nextMonth June = July
+  nextMonth July = August
+  nextMonth August = September
+  nextMonth September = October
+  nextMonth October = November
+  nextMonth November = December
+  nextMonth December = January
+
+  -- c) que retorna a estação do ano de acordo com o mês e com o hemisfério.
+  --    (Use apenas tipos criados pela palavra data aqui.)
+  data Season = Spring | Summer | Autumn | Winter
+  data Hemisphere = North | South
+
+  -- season :: Month, Hemisphere -> Season
+  -- season 
