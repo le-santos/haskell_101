be9f3ee907df79d40d5e1e53e6170b9d3cdbce38
le-santos <lequinterio@gmail.com>
2024-01-22 23:50:12 -0300

    add section3 first solutions

--- exercises_1/section3.hs
+++ exercises_1/section3.hs
@@ -5,3 +5,10 @@ module Section3 where
 
   -- a) Implemente uma função que filtre os números pares e outra
   -- que filtre os ímpares de uma lista recebida via parâmetro.
+  filterEven :: [Int] -> [Int]
+  filterEven [] = []
+  filterEven xs = filter even xs
+  
+  filterOdd :: [Int] -> [Int]
+  filterOdd [] = []
+  filterOdd xs = filter odd xs
