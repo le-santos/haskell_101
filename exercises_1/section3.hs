f23088ffd38242f6a1027dc186269526e2117b07
le-santos <lequinterio@gmail.com>
2024-01-23 00:30:09 -0300

    wip section3

--- exercises_1/section3.hs
+++ exercises_1/section3.hs
@@ -1,7 +1,7 @@
 module Section3 where
-    {-
-      ===== Item 3.1 =====
-    -}
+  {-
+    ===== Item 3.1 =====
+  -}
 
   -- a) Implemente uma função que filtre os números pares e outra
   -- que filtre os ímpares de uma lista recebida via parâmetro.
@@ -12,3 +12,24 @@ module Section3 where
   filterOdd :: [Int] -> [Int]
   filterOdd [] = []
   filterOdd xs = filter odd xs
+
+  {-
+      ===== Item 3.2 =====
+    Implemente o tipo Dinheiro que contenha os campos valor e correncia
+    ( Real ou Dolar ), e uma função que converta todos os ”dinheiros” de uma
+    lista para dólar (e outra para real).
+  -}
+  data Currency = Dolar | Real deriving Show
+  data Money = Money { value :: Double, currency :: Currency} deriving Show
+
+  -- convertMoney :: [Money] -> [Money]
+  -- convertMoney [] = []
+  -- convertMoney ms = map copyMoney ms
+  --   where
+  --     copyMoney :: Money -> Money
+  --     copyMoney (Money v Real) = Money v Dolar
+  --     copyMoney (Money v Dolar) = Money v Real
+
+  -- Com isso, implemente funções para:
+  -- (a) Filtrar todos os Dolares de uma lista de Dinheiro
+
