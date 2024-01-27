d718dfeacb16cb2cbe03a6aeaa89b2e762ca59b5
leandro.quinterio <leandro.quinterio@rebase.com.br>
2024-01-27 10:42:42 -0300

    add section3 solutions

--- exercises_1/section3.hs
+++ exercises_1/section3.hs
@@ -1,34 +1,32 @@
 module Section3 where
-  {-
-    ===== Item 3.1 =====
-  -}
-
+  {- ===== Item 3.1 ===== -}
   -- a) Implemente uma função que filtre os números pares e outra
   -- que filtre os ímpares de uma lista recebida via parâmetro.
+
   filterEven :: [Int] -> [Int]
   filterEven [] = []
   filterEven xs = filter even xs
-  
+
   filterOdd :: [Int] -> [Int]
   filterOdd [] = []
   filterOdd xs = filter odd xs
 
-  {-
-      ===== Item 3.2 =====
-    Implemente o tipo Dinheiro que contenha os campos valor e correncia
-    ( Real ou Dolar ), e uma função que converta todos os ”dinheiros” de uma
-    lista para dólar (e outra para real).
-  -}
-  data Currency = Dolar | Real deriving Show
-  data Money = Money { value :: Double, currency :: Currency} deriving Show
+  {- ===== Item 3.2 ===== -}
+  -- Implemente o tipo Dinheiro que contenha os campos valor e correncia
+  -- ( Real ou Dolar ), e uma função que converta todos os ”dinheiros” de uma
+  -- lista para dólar (e outra para real).
 
-  -- convertMoney :: [Money] -> [Money]
-  -- convertMoney [] = []
-  -- convertMoney ms = map copyMoney ms
-  --   where
-  --     copyMoney :: Money -> Money
-  --     copyMoney (Money v Real) = Money v Dolar
-  --     copyMoney (Money v Dolar) = Money v Real
+  data Currency = Dolar | Real deriving Show
+  data Money = Money { value :: Double, currency :: Currency } deriving Show
+
+  -- Ex: convertMoney [ Money 2.0 Real, Money 3.0 Dolar ]
+  convertMoney :: [Money] -> [Money]
+  convertMoney [] = []
+  convertMoney ms = map copyMoney ms
+    where
+      copyMoney :: Money -> Money
+      copyMoney (Money v Real) = Money v Dolar
+      copyMoney (Money v Dolar) = Money v Real
 
   -- Com isso, implemente funções para:
   -- (a) Filtrar todos os Dolares de uma lista de Dinheiro
