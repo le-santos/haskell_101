3b2a556ef6796d8031fa9c3fad817d5c109e1f4a
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-02-08 19:30:13 -0300

    exercicio 3 - c

--- exercises_2/section3.hs
+++ exercises_2/section3.hs
@@ -1,3 +1,7 @@
+{-
+  import Data.Complex
+-}
+
 module Sect3 where
     -- Mônada I.O.
   {-
@@ -28,3 +32,29 @@ module Sect3 where
     putStrLn "Enter a word to reverse"
     input <- getLine
     putStrLn $ reverse input
+
+    {-
+      (c) Faça um programa que calcule uma equação do segundo grau, a partir dos dados digitados pelo usuário.
+    -}
+
+  -- Função para calcular as raízes de uma equação do segundo grau
+  raizesEquacaoSegundoGrau :: Double -> Double -> Double -> (Complex Double, Complex Double)
+  raizesEquacaoSegundoGrau a b c
+      | delta >= 0 = (x1, x2) 
+      | otherwise = (x1 :+ sqrt(abs delta), x2 :+ (-sqrt(abs delta))) -- Se delta for negativo, raízes complexas
+      where
+          delta = b^2 - 4*a*c
+          x1 = ((-b) + sqrt delta) / (2*a)
+          x2 = ((-b) - sqrt delta) / (2*a)
+
+
+  main :: IO ()
+  main = do
+      putStrLn "Digite o coeficiente 'a' da equação do segundo grau:"
+      coefA <- readLn
+      putStrLn "Digite o coeficiente 'b' da equação do segundo grau:"
+      coefB <- readLn
+      putStrLn "Digite o coeficiente 'c' da equação do segundo grau:"
+      coefC <- readLn
+      let (raiz1, raiz2) = raizesEquacaoSegundoGrau coefA coefB coefC
+      putStrLn $ "As raízes da equação são: " ++ show raiz1 ++ " e " ++ show raiz2
